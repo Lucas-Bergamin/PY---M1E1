62fe3f183fddbd072a235827f2a3fb9fc31f2855
Lucas Bergamin <bergamin.int@gmail.com>
2025-11-24 19:47:11 -0400

    Ex - Conversão de temperatura

---
+++ aula007/ex014.py
@@ -0,0 +1,6 @@
+# --- EXERCICIO TRANSFORMANDO Cº EM FAHRENHEIT.
+
+c = float (input ("Informe a temperatura em °C:"))
+f = (c * 9 / 5) + 32
+
+print ("A temperatura de {}°C corresponde a {}°F.".format(c, f))
