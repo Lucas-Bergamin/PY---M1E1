9b8dd53b3d55924026bc6cdaeed5adbca19d3385
Lucas Bergamin <bergamin.int@gmail.com>
2025-11-24 15:50:32 -0400

    Ex - Conversor de medidas

---
+++ aula007/ex008.py
@@ -0,0 +1,18 @@
+# --- EXERCICIO CONVERSOR DE MEDIDAS
+
+m = float (input ("Uma distância em metros:"))
+
+km = m / 1000
+hm = m / 100
+dam = m / 10
+dm = m * 10
+cm = m * 100
+mm = m * 1000
+
+print ("A medida de {:.1f}m corresponde a:".format(m))
+print ("{}km".format(km))
+print ("{}hm".format(hm))
+print ("{}dam".format(dam))
+print ("{:.0f}dm".format(dm))
+print ("{:.0f}cm".format(cm))
+print ("{:.0f}mm".format(mm))
