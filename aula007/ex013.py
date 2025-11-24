95be8374f41b170d0f22d159334badd8fdcc170e
Lucas Bergamin <bergamin.int@gmail.com>
2025-11-24 19:34:14 -0400

    Ex - Aumento de salário

---
+++ aula007/ex013.py
@@ -0,0 +1,8 @@
+# --- EXERCICIO REAJUSTE SALARIAL
+
+salario = float (input ("Qual é o salário do funcionário? R$"))
+reajuste = salario + (salario * 15 / 100)
+
+print ("-" * 20)
+print ("Um funcionário que ganhava R${}, com 15%"" de aumento, passa a receber R${:.2f}.".format(salario, reajuste))
+print ("-" * 20)
